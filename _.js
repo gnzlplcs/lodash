7d7b15e20f5f3c1b059745a834420de5397be96f
gnzlplcs <palaciosgm@outlook.com>
2021-09-27 15:38:36 -0500

    in-range method done

--- _.js
+++ _.js
@@ -5,6 +5,20 @@ const _ = {
     return clampedValue;
   },
 
+  inRange(number, start, end) {
+    if (end == undefined) {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+    let isInRange = start <= number && number < end;
+    return isInRange;
+  },
+
 };
 
 
