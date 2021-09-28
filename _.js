dc8087bf2c5320499640d02ee60b37e20161f686
gnzlplcs <palaciosgm@outlook.com>
2021-09-28 07:27:48 -0500

    pad and has methods done

--- _.js
+++ _.js
@@ -21,12 +21,38 @@ const _ = {
 
   words(string) {
     return string.split(' ');
-  }
+  },
 
-};
+  pad(string, length) {
+    const stringLength = string.length;
+    let result;
+    if (stringLength >= length) {
+      return string;
+    } else {
+      let spaces = length - stringLength;
+      let startSpace, endSpace = 0;
+      if (spaces == 1) {
+        result = ` ${string}`;
+      } else {
+        if (spaces % 2 == 0) {
+          startSpace = spaces / 2;
+          endSpace = spaces / 2;
+        }
+        else {
+          startSpace = Math.floor(spaces / 2);
+          endSpace = Math.ceil(spaces / 2);
+        }
+        result = `${' '.repeat(startSpace)}${string}${' '.repeat(endSpace)}`;
+      }
+      return result;
+    }
+  },
 
-
-
+  has(obj, key) {
+    return obj[key] !== undefined ? true : false;
+  },
+  
+}
 
 // Do not write or modify code below this line.
 module.exports = _;
